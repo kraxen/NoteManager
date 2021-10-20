d25257aa96a677d34f721a63662c20a7699f01f2
kraxen <axe211411@gmail.com>
2021-10-20 16:25:49 +0300

    Add Note Model (#1)

---
+++ NoteManager/DAL/Memmory Storage/INoteStorage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NoteManager.DAL.Memmory_Storage
+{
+    interface INoteStorage
+    {
+    }
+}
